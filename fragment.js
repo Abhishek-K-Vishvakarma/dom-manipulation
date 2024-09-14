99b18bbf9bc772b793ccccc072aa82209f0a4af7
Abhishek-K-Vishvakarma <github101@gmail.com>
2024-09-15 00:07:43 +0530

    Add new feature in javascript and add style with dom

--- fragment.js
+++ fragment.js
@@ -9,7 +9,19 @@ function Fragmention(){
 
   li.appendChild(del);
   ul.appendChild(li);
+  li.style.color= 'red';
+  li.style.fontSize= '20px'
+  li.style.border= '2px solid yellow';
+  li.style.width= '250px'
+  li.style.backgroundColor= 'green'
+  li.style.float = 'right'
+  del.style.backgroundColor = 'orange';
+  del.style.float = 'right';
+  del.style.boxShadow = '-2px -2px 5px aqua';
+  del.style.cursor = 'pointer';
   
+
+
   del.addEventListener('click', function(){
     li.remove();
   });
